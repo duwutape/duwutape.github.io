9cea2114e9c5ff2da9e208cdc26b5966bd852bf9
Duwutape <quinn.langheld@gmx.de>
2024-11-15 16:45:39 +0100

    removed timetable.js because it's now unused

--- js/timetable.js
+++
@@ -1,8 +0,0 @@
-const toggleTimetable = function () {
-    const timetable = document.getElementById("timetable");
-    if (timetable.style.opacity === "0") {
-        timetable.style.opacity = "1";
-    } else {
-        timetable.style.opacity = "0";
-    }
-}
